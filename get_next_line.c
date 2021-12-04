c8f3b93054bab842a1aa3bafae6edffdd0999f55
ishakzail <ishak.zail.iz@gmail.com>
2021-12-04 20:10:19 +0100

    updated

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: izail <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 11:02:55 by izail             #+#    #+#             */
-/*   Updated: 2021/12/01 12:46:29 by izail            ###   ########.fr       */
+/*   Updated: 2021/12/04 18:32:05 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,31 @@ int	ft_nl_found(char *buff)
 	}
 	return (0);
 }
+char	*ft_save(char *save)
+{
+	int		i;
+	int		c;
+	char	*s;
 
+	i = 0;
+	while (save[i] && save[i] != '\n')
+		i++;
+	if (!save[i])
+	{
+		free(save);
+		return (NULL);
+	}
+	s = (char *)malloc(sizeof(char) * (ft_strlen(save) - i + 1));
+	if (!s)
+		return (NULL);
+	i++;
+	c = 0;
+	while (save[i])
+		s[c++] = save[i++];
+	s[c] = '\0';
+	free(save);
+	return (s);
+}
 char	*ft_get_line(char *s)
 {
 	char	*temp;
@@ -45,48 +69,68 @@ char	*ft_get_line(char *s)
 	return (temp);
 }
 
-char	*ft_read_and_save(int fd, char *rec)
+// char	*ft_read_and_save(int fd, char *rec)
+// {
+// 	char	*buff;
+// 	int		ret;
+
+// 	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+// 	if (!buff)
+// 		return (NULL);
+// 	ret = 1;
+// 	while (ret != 0 && !ft_nl_found(rec))
+// 	{
+// 		ret = read(fd, buff, BUFFER_SIZE);
+// 		if (ret == -1)
+// 		{
+// 			free(buff);
+// 			return (NULL);
+// 		}
+// 		buff[ret] = '\0';
+// 		rec = ft_strjoin(buff,rec);
+// 	}
+// 	free(buff);
+// 	return (rec);
+// }
+char	*get_next_line(int fd)
 {
-	char	*buff;
+	char		*line;
+	static char	*rec;
+	char		*buff;
 	int		ret;
 
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
 	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buff)
 		return (NULL);
 	ret = 1;
-	while (ret != 0)
+	while (ret != 0 && !ft_nl_found(rec))
 	{
+		printf("buff : %s\n",buff);
 		ret = read(fd, buff, BUFFER_SIZE);
+		printf("ret : %d\n",ret);
 		if (ret == -1)
 		{
 			free(buff);
 			return (NULL);
 		}
-		//buff[ret] = '\0';
-		rec = ft_strjoin(rec,buff);
+		buff[ret] = '\0';
+		rec = ft_strjoin(buff,rec);
 	}
 	free(buff);
-	return (rec);
-}
-char	*get_next_line(int fd)
-{
-	char		*line;
-	static char	*rec;
-
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	rec = ft_read_and_save(fd,rec);
-	printf("rec : %s",rec);
+	printf("rec :%s\n",rec);
 	if (!rec)
 		return (NULL);
 	line = ft_get_line(rec);
+	rec = ft_save(rec);
 	return (line);
 }
 int main ()
 {
-		int fd = open("temp.txt",  O_RDONLY);
-		char *f = get_next_line(fd);
-		printf("get : %s",f);
+		int fd = open("file.txt", O_RDONLY);
+		//char *f = get_next_line(fd);
+		printf("get : %s",get_next_line(fd));
 		close(fd);
 		return (0);
 }
